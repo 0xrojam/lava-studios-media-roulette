1e8d126eb516517197aa93ef6e5fd634037815b2
rogpog13 <rogpog13@3ce7a752-956b-17c0-59dc-d77e45338633>
2013-02-17 00:22:04 +0000

    Clear List feature on Edit List page completed

--- MediaRoulette/src/com/LavaStudios/mediaroulette/EditList.java
+++ MediaRoulette/src/com/LavaStudios/mediaroulette/EditList.java
@@ -1,5 +1,7 @@
 package com.LavaStudios.mediaroulette;
+
 import java.io.*;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.*;
@@ -14,27 +16,25 @@ public class EditList extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_list);
-		
+
 		final EditText enterTxt = (EditText) findViewById(R.id.enterText);
 		final EditText displayTxt = (EditText) findViewById(R.id.listText);
-		
-		
+
 		ImageButton enterBtn = (ImageButton) findViewById(R.id.enterButton);
 		ImageButton clearBtn = (ImageButton) findViewById(R.id.clearButton);
-		
-		enterBtn.setOnClickListener(new OnClickListener() {
 
+		enterBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// write on SD card file data in the text box
 				try {
 					File myFile = new File("/sdcard/mediaroulette.txt");
-					if(!myFile.exists()) 
-					  myFile.createNewFile();	
-					
+					if (!myFile.exists())
+						myFile.createNewFile();
+
 					Toast.makeText(getBaseContext(),
-							"Added " + enterTxt.getText(),
-							Toast.LENGTH_SHORT).show();
-					   
+							"Added " + enterTxt.getText(), Toast.LENGTH_SHORT)
+							.show();
+
 					FileInputStream fIn = new FileInputStream(myFile);
 					BufferedReader myReader = new BufferedReader(
 							new InputStreamReader(fIn));
@@ -45,21 +45,43 @@ public class EditList extends Activity {
 					}
 					aBuffer += enterTxt.getText();
 					displayTxt.setText(aBuffer);
-					
+
 					FileOutputStream fOut = new FileOutputStream(myFile);
-					OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+					OutputStreamWriter myOutWriter = new OutputStreamWriter(
+							fOut);
 					myOutWriter.append(aBuffer);
 					myOutWriter.close();
 					fOut.close();
-			    	} catch (Exception e) {
+				} catch (Exception e) {
 					Toast.makeText(getBaseContext(), e.getMessage(),
 							Toast.LENGTH_SHORT).show();
 				}
 				enterTxt.setText("");
 				enterTxt.setHint(null);
-				
+
 			}// onClick
-			}); // btnWriteSDFile
+		}); // entrBtn
+
+		clearBtn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				File myFile = new File("/sdcard/mediaroulette.txt");
+
+				if (myFile.exists())
+					myFile.delete();
+
+				try {
+					myFile.createNewFile();
+				} catch (IOException e) {
+					Toast.makeText(getBaseContext(), e.getMessage(),
+							Toast.LENGTH_SHORT).show();
+				}
+				
+				Toast.makeText(getBaseContext(), "List Cleared",
+						Toast.LENGTH_SHORT).show();
+				displayTxt.setText("");
+			} // onClick
+		}); // clearBtn
+
 	}
 
 	@Override
